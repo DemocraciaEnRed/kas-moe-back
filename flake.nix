7e1e88ec2e9904d683d0436b6f053e51b3bed2aa
fchpriani <sietedosfede@gmail.com>
2021-02-22 14:43:40 -0300

    chore: add stable version inputs

--- flake.nix
+++ flake.nix
@@ -2,13 +2,14 @@
   description = "kas-moe-back development environment reproducible build";
 
   inputs = {
+    stable.url = "github:NixOS/nixpkgs/nixos-20.09";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = inputs:
     let
       system = "x86_64-linux";
-      pkgs = inputs.unstable.legacyPackages.${system};
+      pkgs = inputs.stable.legacyPackages.${system};
       env = pkgs.poetry2nix.mkPoetryEnv { projectDir = ./.; };
     in {
       devShell."${system}" = pkgs.mkShell {

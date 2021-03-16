cb6e4625c11b889c125e30ac7a1dcd7b28f66d24
fchpriani <sietedosfede@gmail.com>
2021-03-15 22:05:57 -0300

    feat: add to voters list service

---
+++ src/services/roll.py
@@ -0,0 +1,37 @@
+import json
+import magic
+from io import BytesIO
+from pandas import read_csv
+from fastapi.encoders import jsonable_encoder
+
+from src.models.roll import Item, collection
+
+
+
+class manage():
+
+    def __init__(self):      
+        """ Check if collection properly exist """
+
+    async def validate(self, upload) -> bool:
+        """ Verify file (extension) magic numbers """
+        return True
+
+
+    async def read(self) -> str:
+        """ Read list from roll collection """
+        data = jsonable_encoder([])
+        test = await collection.find_one({"ID":"85323292"})
+        print(test)
+        for row in await collection.find().to_list(length=5):
+            row.pop("_id")
+            data.append(row)
+        return data
+
+
+    async def write(self, upload) -> bool:
+        """ Write new roll based on CSV/XLS file"""
+        self.validate(upload)
+        contents = read_csv(BytesIO(await upload.read()))
+        data = json.loads(contents.to_json(orient='records'))
+        insert = await collection.insert_many(data)

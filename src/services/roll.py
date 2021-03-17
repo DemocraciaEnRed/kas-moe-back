1321ce468c255174c786ca78ea138345cab08d08
fchpriani <sietedosfede@gmail.com>
2021-03-16 21:09:26 -0300

    feat: add to roll methods

--- src/services/roll.py
+++ src/services/roll.py
@@ -4,25 +4,31 @@ from io import BytesIO
 from pandas import read_csv
 from fastapi.encoders import jsonable_encoder
 
-from src.models.roll import Item, collection
+from src.models.roll import Roll, collection
 
 
 class manage():
 
     def __init__(self):      
         """ Check if collection properly exist """
+        pass
 
 
     async def validate(self, upload) -> bool:
         """ Verify file (extension) magic numbers """
+        extension = magic.from_file(upload)
+        pass
 
 
     async def read(self) -> str:
         """ Read list from roll collection """
         data = jsonable_encoder([])
-        for row in await collection.find().to_list(length=5):
-            row.pop("_id")
-            data.append(row)
+        async for roll in Roll.all():
+            data.append({
+                "id": roll.id,
+                "name": roll.name
+            })
+        data = json.dumps(data)
         return data
 
 
@@ -32,3 +38,8 @@ class manage():
         contents = read_csv(BytesIO(await upload.read()))
         data = json.loads(contents.to_json(orient='records'))
         insert = await collection.insert_many(data)
+
+
+    async def clear(self) -> None:
+        """ Clear roll collection """
+        await collection.delete_many({})
